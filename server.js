c6fedb1719006ce951b67605ed92c7aa21b28d10
CHAD AUSTIN WHITE <chadaustinwhite@chads-imac.attlocal.net>
2018-11-15 23:42:20 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.listen(PORT, console.log("Server is running on PORT " + PORT));
